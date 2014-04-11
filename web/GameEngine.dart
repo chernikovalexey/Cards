7506b95c820737ad2cca700f687c3efde893fe0b
Oleksii Chernikov <chernikov.lesha@gmail.com>
2014-04-11 15:00:50 +0300

    Coordinates fixed

--- web/GameEngine.dart
+++ web/GameEngine.dart
@@ -5,27 +5,32 @@ import 'dart:math' as Math;
 import 'package:box2d/box2d_browser.dart';
 
 class GameEngine {
+  static const double WIDTH = 800.0;
+  static const double HEIGHT = 600.0;
+
+  static const double ZERO_GRAVITY = 0.0;
+  static const double NORMAL_GRAVITY = -9.8;
+
+  num lastStepTime = 0;
+
+  World world;
   CanvasRenderingContext2D g;
-
-  num time = 0;
-
-  World world = new World(new Vector2(0.0, -9.8), true, new DefaultWorldPool());
-  List<Body> bodies = new List<Body>();
-
   ViewportTransform viewport;
   DebugDraw debugDraw;
 
+  List<Body> bodies = new List<Body>();
+
   GameEngine(CanvasRenderingContext2D g) {
     this.g = g;
-    
-    initializeCanvas();
+
     initializeWorld();
+    initializeCanvas();
   }
 
   void initializeCanvas() {
-    final extents = new Vector2(800.0 / 2.0, 800.0 / 2.0);
+    Vector2 extents = new Vector2(WIDTH / 2.0, HEIGHT / 2.0);
     viewport = new CanvasViewportTransform(extents, extents);
-    viewport.scale = 10.0;
+    viewport.scale = 1.0;
 
     debugDraw = new CanvasDraw(viewport, g);
 
@@ -34,33 +39,17 @@ class GameEngine {
   }
 
   void initializeWorld() {
+    world = new World(new Vector2(0.0, NORMAL_GRAVITY), true,
+        new DefaultWorldPool());
+    
     // Create the ground.
     PolygonShape sd = new PolygonShape();
-    sd.setAsBox(50.0, 0.4);
+    sd.setAsBox(WIDTH/2, 20.0/2);
 
     BodyDef bd = new BodyDef();
-    bd.position = new Vector2(0.0, 0.0);
-    bd.angle = Math.PI / 8;
+    bd.position = new Vector2(0.0, -HEIGHT/2);
     Body ground = world.createBody(bd);
     ground.createFixtureFromShape(sd);
-
-    // Create a bouncing ball.
-    final bouncingBall = new CircleShape();
-    bouncingBall.radius = 5.0;
-
-    final ballFixtureDef = new FixtureDef();
-    ballFixtureDef.restitution = 0.7;
-    ballFixtureDef.density = 0.05;
-    ballFixtureDef.shape = bouncingBall;
-
-    final ballBodyDef = new BodyDef();
-    ballBodyDef.linearVelocity = new Vector2(-4.5, -2.0);
-    ballBodyDef.position = new Vector2(15.0, 15.0);
-    ballBodyDef.type = BodyType.DYNAMIC;
-    ballBodyDef.bullet = true;
-
-    final ballBody = world.createBody(ballBodyDef);
-    ballBody.createFixture(ballFixtureDef);
   }
 
   void run() {
@@ -68,16 +57,16 @@ class GameEngine {
   }
 
   void step(num time) {
-    num delta = time - this.time;
+    num delta = time - this.lastStepTime;
 
-    const num TIME_STEP = 1 / 60;
-    const int VELOCITY_ITERATIONS = 10;
-    const int POSITION_ITERATIONS = 10;
-    world.step(TIME_STEP, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
-    g.clearRect(0, 0, 800, 800);
+    world.step(1 / 60, 10, 10);
+
+    g.setFillColorRgb(0, 0, 0);
+    g.fillRect(0, 0, 800, 800);
+
     world.drawDebugData();
 
-    this.time = time;
+    this.lastStepTime = time;
 
     run();
   }
