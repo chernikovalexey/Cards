d29c69d4c89ad232e2b606534fe56bed4cda9c91
Oleksii Chernikov <chernikov.lesha@gmail.com>
2014-06-16 16:00:05 +0300

    disabled elements underneath blur

--- web/BoundedCard.dart
+++ web/BoundedCard.dart
@@ -8,7 +8,6 @@ import "Color4.dart";
 
 class BoundedCard {
   Body b;
-
   GameEngine e;
 
   BoundedCard(GameEngine e) {
@@ -32,21 +31,22 @@ class BoundedCard {
   }
 
   void update() {
-
     double angle = b.angle;
     angle += Input.wheelDirection * Math.PI / 24;
     b.setTransform(new Vector2(Input.mouseX, Input.mouseY), angle);
 
     Color4 col;
-    if (e.staticBlocksSelected) {
+    if(!e.contactListener.contactingBodies.isEmpty) {
+      col = new Color4.fromRGBA(255, 0, 0, 0.5);
+    } else if (e.staticBlocksSelected) {
       col = new Color4.fromRGB(217, 214, 179);
-      if (e.physicsEnabled || e.level.current.staticBlocksRemaining==0) col = new Color4.fromRGB(134, 133, 119);
+      if (e.physicsEnabled || e.level.current.staticBlocksRemaining == 0) col =
+          new Color4.fromRGB(134, 133, 119);
     } else {
       col = new Color4.fromRGB(234, 140, 64);
-      if (e.physicsEnabled || e.level.current.dynamicBlocksRemaining==0) col = new Color4.fromRGB(113, 86, 64);
+      if (e.physicsEnabled || e.level.current.dynamicBlocksRemaining == 0) col =
+          new Color4.fromRGB(113, 86, 64);
     }
-
-
 
     (b.userData as Sprite).color = col;
   }
