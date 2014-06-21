04413dcf7f651870d039e25f958f125bbbf5fcbe
Oleksii Chernikov <chernikov.lesha@gmail.com>
2014-06-21 14:16:53 +0300

    navigation thru screens (tutorial)

--- web/GameWizard.dart
+++ web/GameWizard.dart
@@ -1,15 +1,17 @@
 import 'dart:html';
 import "cards.dart";
 import 'Tooltip.dart';
-import 'dart:async';
 
 class GameWizard {
+  static const int ANIM_TIME = 175;
+
   static Element currentBox; // current box
   static Element progress = querySelector(".tutorial-progress");
 
   static const String DYNAMIC =
       "The type of block to put. Pay attention to the amount of blocks remaining.<br><br>Dynamic blocks convey energy and fall when physics applied.";
-  static const String STATIC = "Static blocks. They don't fall when physics applied. But, as well, they <b>don't convey the energy</b>.";
+  static const String STATIC =
+      "Static blocks. They don't fall when physics applied. But, as well, they <b>don't convey the energy</b>.";
   static const String ZOOM =
       "Use two these buttons to zoom camera in and out, accordingly.";
   static const String TOGGLE_PHYSICS =
@@ -18,13 +20,23 @@ class GameWizard {
   static bool showing = false;
 
   static void enterStep(Element box) {
-    fadeBoxOut(currentBox, 175, () {
-      fadeBoxIn(box);
-    });
-    querySelectorAll(".progress-step").forEach((Element el){
-      el.classes.remove("active-step");
-    });
-    Element stepBullet = querySelector("." + box.dataset['related']);
+    if (currentBox != box) {
+      var callback = () {
+        fadeBoxIn(box);
+        currentBox = box;
+      };
+
+      if (currentBox != null) {
+        fadeBoxOut(currentBox, ANIM_TIME, callback);
+      } else {
+        callback();
+      }
+
+      querySelectorAll(".progress-step").forEach((Element el) {
+        el.classes.remove("active-step");
+      });
+      querySelector(".tutorial-progress ." + box.id).classes.add("active-step");
+    }
   }
 
   static void finish() {
@@ -37,9 +49,8 @@ class GameWizard {
     active.nextElementSibling.classes.add("active-step");
   }
 
-  static void skip(Event event) {
-    fadeBoxOut(box);
-    Tooltip.closeAll();
+  static bool isTutorial(int chapter, int level) {
+    return true;
   }
 
   static void manage(int chapter, int level) {
@@ -53,12 +64,11 @@ class GameWizard {
   static void showOverview() {
     showing = true;
 
-    Element greeting = querySelector("#game-wizard");
-
     fadeBoxIn(progress);
-    fadeBoxIn(greeting);
+    enterStep(querySelector("#wizard-overview"));
 
-    Tooltip.show(querySelector(".plus"), ZOOM, Tooltip.BOTTOM, maxWidth: 300, xOffset: -79, xArrowOffset: 24);
+    Tooltip.show(querySelector(".plus"), ZOOM, Tooltip.BOTTOM, maxWidth: 300,
+        xOffset: -79, xArrowOffset: 24);
     Tooltip.show(querySelector("#toggle-physics"), TOGGLE_PHYSICS,
         Tooltip.BOTTOM, maxWidth: 300, xOffset: 91);
 
@@ -79,31 +89,19 @@ class GameWizard {
       }
     });
 
-    int speed = 175;
-    Element controlsBox = querySelector("#controls");
-
-    querySelector(".show-controls").addEventListener("click", (event) {
-      fadeBoxOut(greeting, speed, () {
-        fadeBoxIn(controlsBox, speed);
-        nextStep();
-      });
-    }, false);
-
-    querySelector(".try-button").addEventListener("click", (event) {
-      fadeBoxOut(controlsBox, speed, () {
-        Tooltip.show(querySelector(".dynamic"), DYNAMIC, Tooltip.RIGHT, maxWidth: 300, yOffset: -9, yArrowOffset: -12);
-      });
-      nextStep();
-    }, false);
-
     querySelectorAll(".progress-step").onClick.listen((event) {
-      if (event.target.classes.contains("overview")) {
-        
+      if (event.target.classes.contains("wizard-overview")) {
+        enterStep(querySelector("#wizard-overview"));
+      } else if (event.target.classes.contains("wizard-controls")) {
+        enterStep(querySelector("#wizard-controls"));
+      } else if (event.target.classes.contains("wizard-try")) {
+        enterStep(querySelector("#wizard-try"));
       }
     });
   }
 
   static void showStaticAppear() {
-    Tooltip.show(querySelector(".static"), STATIC, Tooltip.RIGHT, maxWidth: 300, yOffset: -9, yArrowOffset: -12);
+    Tooltip.show(querySelector(".static"), STATIC, Tooltip.RIGHT, maxWidth: 300,
+        yOffset: -9, yArrowOffset: -12);
   }
 }
