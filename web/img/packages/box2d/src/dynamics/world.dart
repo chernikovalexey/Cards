588ad1e710c1a1d0fa9a2b0028546936c6b0a41d
chernikovalexey <SBwbralte12qwer34enex14>
2015-01-14 18:22:59 +0200

    Packages updated.

--- web/img/packages/box2d/src/dynamics/world.dart
+++ web/img/packages/box2d/src/dynamics/world.dart
@@ -1,11 +1,11 @@
 // Copyright 2012 Google Inc. All Rights Reserved.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -153,6 +153,11 @@ class World {
       register2.primary = false;
       _contactStacks[type2][type1] = register2;
     }
+  }
+
+  void setGravity(Vector2 gravity) {
+    _gravity.x = gravity.x;
+    _gravity.y = gravity.y;
   }
 
   void _initializeRegisters() {
