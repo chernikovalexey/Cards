377ad583cdcc90b20dd02edd4125b34f93dca679
podkolzzzin <podkolzzzin@yandex.ru>
2020-06-02 05:54:00 +0300

    Change title and set social media title

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<?header('Location: /web/cards.html');
+<?header('Location: /web/cards.html');?>
+<html>
+    <head>
+        <meta name="twitter:title" content="Two Cubes – the web puzzle game."/>
+        <meta property="og:title" content="Two Cubes – the web puzzle game."/>
+        
+        <meta name="description" content="Overcome the laws of physics and connect blocks.">
+        <meta name="twitter:description" content="Overcome the laws of physics and connect blocks."/>
+        <meta property="og:description" content="Overcome the laws of physics and connect blocks."/>
+
+        <meta property="og:image:secure_url" content="cubes.png"/>
+        <meta property="og:image" content="cubes.png"/>
+        <meta name="twitter:image" content="cubes.png"/>
+        <title>Two Cubes – the web puzzle game.</title>
+    </head>
+</html>
